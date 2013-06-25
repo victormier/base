e9c044b3fdc0aee1543838adfe81f9638c62a53b
Victor Mier <victormier@gmail.com>
2013-06-25 21:57:56 +0200

    Add search to translations and some styles

--- app/controllers/admin/translations_controller.rb
+++ app/controllers/admin/translations_controller.rb
@@ -1,6 +1,20 @@
 class Admin::TranslationsController < Admin::ApplicationController
   def index
-    @translations = Interpret::Translation.order('translations.key').paginate(:page => params[:page], :per_page => 20)
+    @translations = Interpret::Translation.scoped
+    @search_params = Hash["key" => "", "value" => "", "language" => "All"]
+
+    if params[:search].present?
+      @search_params = params[:search]
+      t = Interpret::Translation.arel_table
+      @translations = @translations.where(t[:key].
+        matches("%#{params[:search][:key]}%").
+        and(t[:value].matches("%#{params[:search][:value]}%")))
+      if LOCALES.include?(params[:search][:language].to_sym)
+        @translations = @translations.where(t[:locale].eq("#{params[:search][:language]}"))
+      end
+    end
+
+    @translations = @translations.order('translations.key').paginate(:page => params[:page], :per_page => 20)
   end
 
   def other_langs
