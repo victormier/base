619f59387e7a8397762d26492e7e8240c1cff852
Victor Mier <victormier@gmail.com>
2013-06-25 21:47:00 +0200

    Add basic best in place editable translations table

--- app/controllers/admin/translations_controller.rb
+++ app/controllers/admin/translations_controller.rb
@@ -1,4 +1,5 @@
 class Admin::TranslationsController < Admin::ApplicationController
   def index
+    @translations = Interpret::Translation.order('translations.key').paginate(:page => params[:page], :per_page => 20)
   end
 end
