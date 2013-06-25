dfee3ff556318634719821a53f8e2fcf80a8c6a9
Victor Mier <victormier@gmail.com>
2013-06-25 21:40:28 +0200

    Add languages initializer

---
+++ config/initializers/languages.rb
@@ -0,0 +1,12 @@
+# encoding: utf-8
+
+MAIN_LOCALE = :en
+
+LANGUAGES_BY_LOCALE = {
+  "en" => 'English',
+  "es" => 'Castellano'
+}
+
+LANGUAGES = LANGUAGES_BY_LOCALE.to_a.map(&:reverse)
+LOCALES = LANGUAGES_BY_LOCALE.keys.map(&:to_sym)
+TRANSLATABLE_LOCALES = LOCALES - [MAIN_LOCALE]
