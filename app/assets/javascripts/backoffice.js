81595a3db217d1273cda4ab07193ad98f02364e9
Victor Mier <victormier@gmail.com>
2013-07-25 18:30:26 +0200

    Load tab js from bootstrap

--- app/assets/javascripts/backoffice.js
+++ app/assets/javascripts/backoffice.js
@@ -3,5 +3,6 @@
 //= require best_in_place
 //= require twitter/bootstrap/bootstrap-alert
 //= require twitter/bootstrap/bootstrap-modal
+//= require twitter/bootstrap/bootstrap-tab
 //= require redactor
 //= require admin/end.js
